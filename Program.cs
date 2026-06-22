e7e1bc102cc513208158ec3ee079e78220f2ed4a
juancitholc-del <juancitho.lc@gmail.com>
2026-06-21 22:49:37 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+  
+    static double CalcularSueldo(double horas, double pagoHora)
+    {
+        double sueldo = horas * pagoHora;
+        return sueldo;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("=== CÁLCULO DE SUELDO ===");
+
+        Console.Write("Ingrese horas trabajadas: ");
+        double horas = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese pago por hora: ");
+        double pagoHora = Convert.ToDouble(Console.ReadLine());
+        double sueldoFinal = CalcularSueldo(horas, pagoHora);
+
+        Console.WriteLine("El sueldo del trabajador es: S/ " + sueldoFinal);
+        Console.ReadKey();
+    }
+    
+}
